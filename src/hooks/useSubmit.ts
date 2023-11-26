c28241a3d5bb642282a8c297f569aead6d24b8a8
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2023-11-26 12:22:28 -0800

    smoother streaming

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -52,157 +52,302 @@ const useSubmit = () => {
   };
 
   const handleSubmit = async () => {
-    const chats = useStore.getState().chats;
-    if (generating || !chats) return;
+  const chats = useStore.getState().chats;
+  if (generating || !chats) return;
 
-    const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(chats));
+  const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(chats));
 
-    updatedChats[currentChatIndex].messages.push({
-      role: 'assistant',
-      content: '',
-    });
+  updatedChats[currentChatIndex].messages.push({
+    role: 'assistant',
+    content: '',
+  });
 
-    setChats(updatedChats);
-    setGenerating(true);
+  setChats(updatedChats);
+  setGenerating(true);
 
-    try {
-      let stream;
-      if (chats[currentChatIndex].messages.length === 0)
-        throw new Error('No messages submitted!');
+  try {
+    let stream;
+    if (chats[currentChatIndex].messages.length === 0)
+      throw new Error('No messages submitted!');
 
-      const messages = limitMessageTokens(
-        chats[currentChatIndex].messages,
-        chats[currentChatIndex].config.max_tokens,
-        chats[currentChatIndex].config.model
+    const messages = limitMessageTokens(
+      chats[currentChatIndex].messages,
+      chats[currentChatIndex].config.max_tokens,
+      chats[currentChatIndex].config.model
+    );
+    if (messages.length === 0) throw new Error('Message exceed max token!');
+
+    if (!apiKey || apiKey.length === 0) {
+      if (apiEndpoint === officialAPIEndpoint) {
+        throw new Error(t('noApiKeyWarning') as string);
+      }
+
+      stream = await getChatCompletionStream(
+        useStore.getState().apiEndpoint,
+        messages,
+        chats[currentChatIndex].config
       );
-      if (messages.length === 0) throw new Error('Message exceed max token!');
+    } else if (apiKey) {
+      stream = await getChatCompletionStream(
+        useStore.getState().apiEndpoint,
+        messages,
+        chats[currentChatIndex].config,
+        apiKey
+      );
+    }
 
-      // no api key (free)
-      if (!apiKey || apiKey.length === 0) {
-        // official endpoint
-        if (apiEndpoint === officialAPIEndpoint) {
-          throw new Error(t('noApiKeyWarning') as string);
-        }
+    if (stream) {
+      if (stream.locked)
+        throw new Error('Oops, the stream is locked right now. Please try again');
+      const reader = stream.getReader();
+      let reading = true;
+      let partial = '';
 
-        // other endpoints
-        stream = await getChatCompletionStream(
-          useStore.getState().apiEndpoint,
-          messages,
-          chats[currentChatIndex].config
+      while (reading && useStore.getState().generating) {
+        const { done, value } = await reader.read();
+        const result = parseEventSource(
+          partial + new TextDecoder().decode(value)
         );
-      } else if (apiKey) {
-        // own apikey
-        stream = await getChatCompletionStream(
-          useStore.getState().apiEndpoint,
-          messages,
-          chats[currentChatIndex].config,
-          apiKey
-        );
-      }
+        partial = '';
 
-      if (stream) {
-        if (stream.locked)
-          throw new Error(
-            'Oops, the stream is locked right now. Please try again'
-          );
-        const reader = stream.getReader();
-        let reading = true;
-        let partial = '';
-        while (reading && useStore.getState().generating) {
-          const { done, value } = await reader.read();
-          const result = parseEventSource(
-            partial + new TextDecoder().decode(value)
-          );
-          partial = '';
-
-          if (result === '[DONE]' || done) {
-            reading = false;
-          } else {
-            const resultString = result.reduce((output: string, curr) => {
-              if (typeof curr === 'string') {
-                partial += curr;
-              } else {
-                const content = curr.choices[0].delta.content;
-                if (content) output += content;
-              }
-              return output;
-            }, '');
-
-            const updatedChats: ChatInterface[] = JSON.parse(
-              JSON.stringify(useStore.getState().chats)
-            );
-            const updatedMessages = updatedChats[currentChatIndex].messages;
-            updatedMessages[updatedMessages.length - 1].content += resultString;
-            setChats(updatedChats);
-          }
-        }
-        if (useStore.getState().generating) {
-          reader.cancel('Cancelled by user');
+        if (result === '[DONE]' || done) {
+          reading = false;
         } else {
-          reader.cancel('Generation completed');
-        }
-        reader.releaseLock();
-        stream.cancel();
-      }
+          const resultString = result.reduce((output: string, curr) => {
+            if (typeof curr === 'string') {
+              partial += curr;
+            } else {
+              const content = curr.choices[0].delta.content;
+              if (content) output += content;
+            }
+            return output;
+          }, '');
 
-      // update tokens used in chatting
-      const currChats = useStore.getState().chats;
-      const countTotalTokens = useStore.getState().countTotalTokens;
+          // TODO: have this dynamically update
+          const delayPerCharacter = 4;
+          let delay = 0;
 
-      if (currChats && countTotalTokens) {
-        const model = currChats[currentChatIndex].config.model;
-        const messages = currChats[currentChatIndex].messages;
-        updateTotalTokenUsed(
-          model,
-          messages.slice(0, -1),
-          messages[messages.length - 1]
-        );
-      }
-
-      // generate title for new chats
-      if (
-        useStore.getState().autoTitle &&
-        currChats &&
-        !currChats[currentChatIndex]?.titleSet
-      ) {
-        const messages_length = currChats[currentChatIndex].messages.length;
-        const assistant_message =
-          currChats[currentChatIndex].messages[messages_length - 1].content;
-        const user_message =
-          currChats[currentChatIndex].messages[messages_length - 2].content;
-
-        const message: MessageInterface = {
-          role: 'user',
-          content: `Generate a title in less than 6 words for the following message (language: ${i18n.language}):\n"""\nUser: ${user_message}\nAssistant: ${assistant_message}\n"""`,
-        };
-
-        let title = (await generateTitle([message])).trim();
-        if (title.startsWith('"') && title.endsWith('"')) {
-          title = title.slice(1, -1);
-        }
-        const updatedChats: ChatInterface[] = JSON.parse(
-          JSON.stringify(useStore.getState().chats)
-        );
-        updatedChats[currentChatIndex].title = title;
-        updatedChats[currentChatIndex].titleSet = true;
-        setChats(updatedChats);
-
-        // update tokens used for generating title
-        if (countTotalTokens) {
-          const model = _defaultChatConfig.model;
-          updateTotalTokenUsed(model, [message], {
-            role: 'assistant',
-            content: title,
+          resultString.split('').forEach((character) => {
+            setTimeout(() => {
+              const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(useStore.getState().chats));
+              const updatedMessages = updatedChats[currentChatIndex].messages;
+              updatedMessages[updatedMessages.length - 1].content += character;
+              setChats(updatedChats);
+            }, delay);
+            delay += delayPerCharacter;
           });
         }
       }
-    } catch (e: unknown) {
-      const err = (e as Error).message;
-      console.log(err);
-      setError(err);
+
+      if (useStore.getState().generating) {
+        reader.cancel('Cancelled by user');
+      } else {
+        reader.cancel('Generation completed');
+      }
+      reader.releaseLock();
+      stream.cancel();
     }
-    setGenerating(false);
-  };
+
+    const currChats = useStore.getState().chats;
+    const countTotalTokens = useStore.getState().countTotalTokens;
+
+    if (currChats && countTotalTokens) {
+      const model = currChats[currentChatIndex].config.model;
+      const messages = currChats[currentChatIndex].messages;
+      updateTotalTokenUsed(
+        model,
+        messages.slice(0, -1),
+        messages[messages.length - 1]
+      );
+    }
+
+    if (useStore.getState().autoTitle && currChats && !currChats[currentChatIndex]?.titleSet) {
+      const messages_length = currChats[currentChatIndex].messages.length;
+      const assistant_message = currChats[currentChatIndex].messages[messages_length - 1].content;
+      const user_message = currChats[currentChatIndex].messages[messages_length - 2].content;
+
+      const message: MessageInterface = {
+        role: 'user',
+        content: `Generate a title in less than 6 words for the following message (language: ${i18n.language}):\n"""\nUser: ${user_message}\nAssistant: ${assistant_message}\n"""`,
+      };
+
+      let title = (await generateTitle([message])).trim();
+      if (title.startsWith('"') && title.endsWith('"')) {
+        title = title.slice(1, -1);
+      }
+      const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(useStore.getState().chats));
+      updatedChats[currentChatIndex].title = title;
+      updatedChats[currentChatIndex].titleSet = true;
+      setChats(updatedChats);
+
+      if (countTotalTokens) {
+        const model = _defaultChatConfig.model;
+        updateTotalTokenUsed(model, [message], { role: 'assistant', content: title });
+      }
+    }
+
+  } catch (e: unknown) {
+    const err = (e as Error).message;
+    console.log(err);
+    setError(err);
+  }
+
+  setGenerating(false);
+};
+
+
+  // const handleSubmit = async () => {
+  //   const chats = useStore.getState().chats;
+  //   if (generating || !chats) return;
+
+  //   const updatedChats: ChatInterface[] = JSON.parse(JSON.stringify(chats));
+
+  //   updatedChats[currentChatIndex].messages.push({
+  //     role: 'assistant',
+  //     content: '',
+  //   });
+
+  //   setChats(updatedChats);
+  //   setGenerating(true);
+
+  //   try {
+  //     let stream;
+  //     if (chats[currentChatIndex].messages.length === 0)
+  //       throw new Error('No messages submitted!');
+
+  //     const messages = limitMessageTokens(
+  //       chats[currentChatIndex].messages,
+  //       chats[currentChatIndex].config.max_tokens,
+  //       chats[currentChatIndex].config.model
+  //     );
+  //     if (messages.length === 0) throw new Error('Message exceed max token!');
+
+  //     // no api key (free)
+  //     if (!apiKey || apiKey.length === 0) {
+  //       // official endpoint
+  //       if (apiEndpoint === officialAPIEndpoint) {
+  //         throw new Error(t('noApiKeyWarning') as string);
+  //       }
+
+  //       // other endpoints
+  //       stream = await getChatCompletionStream(
+  //         useStore.getState().apiEndpoint,
+  //         messages,
+  //         chats[currentChatIndex].config
+  //       );
+  //     } else if (apiKey) {
+  //       // own apikey
+  //       stream = await getChatCompletionStream(
+  //         useStore.getState().apiEndpoint,
+  //         messages,
+  //         chats[currentChatIndex].config,
+  //         apiKey
+  //       );
+  //     }
+
+  //     if (stream) {
+  //       if (stream.locked)
+  //         throw new Error(
+  //           'Oops, the stream is locked right now. Please try again'
+  //         );
+  //       const reader = stream.getReader();
+  //       let reading = true;
+  //       let partial = '';
+  //       while (reading && useStore.getState().generating) {
+  //         const { done, value } = await reader.read();
+  //         const result = parseEventSource(
+  //           partial + new TextDecoder().decode(value)
+  //         );
+  //         partial = '';
+
+  //         if (result === '[DONE]' || done) {
+  //           reading = false;
+  //         } else {
+  //           const resultString = result.reduce((output: string, curr) => {
+  //             if (typeof curr === 'string') {
+  //               partial += curr;
+  //             } else {
+  //               const content = curr.choices[0].delta.content;
+  //               if (content) output += content;
+  //             }
+  //             return output;
+  //           }, '');
+
+  //           const updatedChats: ChatInterface[] = JSON.parse(
+  //             JSON.stringify(useStore.getState().chats)
+  //           );
+  //           const updatedMessages = updatedChats[currentChatIndex].messages;
+  //           updatedMessages[updatedMessages.length - 1].content += resultString;
+  //           setChats(updatedChats);
+  //         }
+  //       }
+  //       if (useStore.getState().generating) {
+  //         reader.cancel('Cancelled by user');
+  //       } else {
+  //         reader.cancel('Generation completed');
+  //       }
+  //       reader.releaseLock();
+  //       stream.cancel();
+  //     }
+
+  //     // update tokens used in chatting
+  //     const currChats = useStore.getState().chats;
+  //     const countTotalTokens = useStore.getState().countTotalTokens;
+
+  //     if (currChats && countTotalTokens) {
+  //       const model = currChats[currentChatIndex].config.model;
+  //       const messages = currChats[currentChatIndex].messages;
+  //       updateTotalTokenUsed(
+  //         model,
+  //         messages.slice(0, -1),
+  //         messages[messages.length - 1]
+  //       );
+  //     }
+
+  //     // generate title for new chats
+  //     if (
+  //       useStore.getState().autoTitle &&
+  //       currChats &&
+  //       !currChats[currentChatIndex]?.titleSet
+  //     ) {
+  //       const messages_length = currChats[currentChatIndex].messages.length;
+  //       const assistant_message =
+  //         currChats[currentChatIndex].messages[messages_length - 1].content;
+  //       const user_message =
+  //         currChats[currentChatIndex].messages[messages_length - 2].content;
+
+  //       const message: MessageInterface = {
+  //         role: 'user',
+  //         content: `Generate a title in less than 6 words for the following message (language: ${i18n.language}):\n"""\nUser: ${user_message}\nAssistant: ${assistant_message}\n"""`,
+  //       };
+
+  //       let title = (await generateTitle([message])).trim();
+  //       if (title.startsWith('"') && title.endsWith('"')) {
+  //         title = title.slice(1, -1);
+  //       }
+  //       const updatedChats: ChatInterface[] = JSON.parse(
+  //         JSON.stringify(useStore.getState().chats)
+  //       );
+  //       updatedChats[currentChatIndex].title = title;
+  //       updatedChats[currentChatIndex].titleSet = true;
+  //       setChats(updatedChats);
+
+  //       // update tokens used for generating title
+  //       if (countTotalTokens) {
+  //         const model = _defaultChatConfig.model;
+  //         updateTotalTokenUsed(model, [message], {
+  //           role: 'assistant',
+  //           content: title,
+  //         });
+  //       }
+  //     }
+  //   } catch (e: unknown) {
+  //     const err = (e as Error).message;
+  //     console.log(err);
+  //     setError(err);
+  //   }
+  //   setGenerating(false);
+  // };
 
   return { handleSubmit, error };
 };
